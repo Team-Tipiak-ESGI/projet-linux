cedc3d4f51552eacd356c948b97d73c6af949619
astalios <astalios@github.com>
2021-06-28 10:33:32 +0000

    modif useradd

--- bulk-useradd.sh
+++ bulk-useradd.sh
@@ -1,20 +1,20 @@
 #!/bin/bash/
 
-####################################################
-#
-# useradd - ESGI edition
-# useradd prend en entree un fichier
-# Il permet de creer de facon automatique des utilisateurs utilisables
-# structure : login:surname:name:group1:group2:...:groupN:password
-# group1 = groupe primaire
-# group 2 -> n = groupes secondaires
-# mot de passe expirable
-#
-####################################################
+echo "###########################################################################"
+echo "#										#"
+echo "# useradd - ESGI edition							#"
+echo "# useradd prend en entree un fichier					#"
+echo "# Il permet de creer de facon automatique des utilisateurs utilisables	#"
+echo "# structure : login:surname:name:group1:group2:...:groupN:password	#"
+echo "# group1 = groupe primaire						#"
+echo "# group 2 -> n = groupes secondaires					#"
+echo "# mot de passe expirable							#"
+echo "#										#"
+echo "###########################################################################"
 
 flags_home="--create-home " # creation du home @/home/user
 flags_shell="--shell /bin/bash " # affectation du shell bash a l'user, on peux mettre zsh pour plus de flex
-flags_groups="--groups $groups " # affectations des groups TODO: A verifier au prealable si les groupes sont pas au prealable crees
+flags_groups="--groups $groups " # affectations des groupes TODO: A verifier au prealable si les groupes sont pas au prealable crees
 flags_primary="--gid $groups[1] " # groups etant un tableau de group1 -> groupN
 flags_password="--password $(echo $password | openssl passwd -crypt -stdin)" # voir le man de useradd et passwd
 
