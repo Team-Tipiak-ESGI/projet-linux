6c292e20ad9afe6d8d904f21cf6369f47931afca
HeartBtz <sebastienange78@gmail.com>
2021-06-30 15:58:32 +0200

    script-affichage-ok

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 outerr (){
 	echo "$@" >&2
@@ -8,31 +9,27 @@ then
 	outerr "Abandon..."
 	exit 1
 fi
-var=($(du -s /home/* | sort -nr))
+var=($(du -sb /home/* | sort -nr))
 printf "top 5 des utilisateurs les plus gourmands :\n"
 for ((i=0 ; 10 - $i ; i++))
 do
 	if [ $((i%2)) -eq 0 ]
 	then
 		temp=$((${var[$i]}))
-		var[$i]=$(($temp * 1024))
-		#echo ${var[$i]}
-		ko=$temp
-		mo=$(($ko - $ko/1024))
-		#go=$(($mo - $mo/1024))
-		#to=$(($go - $go/1024))
-		#echo $ko
-		#echo $mo
-		#echo $go
-		#echo $to
-	else
-		username=$(echo ${var[$i]} | cut -d/ -f3)
-		printf "%s\n" "$username"
+		to=$(($temp / 1099511627776))
+		go=$((($temp - $to * 1099511627776) / 1073741824))
+		mo=$((($temp - $go * 1073741824) / 1048576))
+		ko=$((($temp - $mo * 1048576) / 1024))
+		o=$(($temp - $to * 1099511627776 - $go * 1073741824 - $mo * 1048576 - $ko * 1024 ))
+		#ko=$(($temp / 1024))
+		#go=$(($temp / 1073741824))
+		#mo=$(($temp / 1048576))
 fi
-	if [ $((i%2)) -eq 0 ]
+	if [ $((i%2)) -eq 1 ]
 	then
-		a="a"
-		#echo 
+		username=$(echo ${var[$i]} | cut -d/ -f3)
+		count=$(($i / 2 + 1))
+		printf "%d  - %d Go, %d Mo, %d ko, %d octets - %s\n" "$count" "$go" "$mo" "$ko" "$o" "$username"
 	fi
 done
 
